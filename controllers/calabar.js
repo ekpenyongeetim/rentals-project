b2015029a74dbd303d9de000300ab6758846c61c
ekpenyongeetim <ekpenyongeetim@gmail.com>
2024-02-09 20:32:10 +0100

    create, update and delete endpoint

--- controllers/calabar.js
+++ controllers/calabar.js
@@ -22,7 +22,70 @@ const getSingle = async (req, res) => {
   });
 };
 
+const createRental = async (req, res) => {
+  const rental = {
+    id: req.body.id,
+    photos: req.body.photos,
+    address: req.body.address,
+    city: req.body.city,
+    rating: req.body.city,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("calabar")
+    .insertOne(rental);
+  if (response.acknowledge) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while updating the rental.");
+  }
+};
+
+const updateRental = async (req, res) => {
+  const calabarId = new ObjectId(req.params.id);
+  const rental = {
+    id: req.body.id,
+    photos: req.body.photos,
+    address: req.body.address,
+    city: req.body.city,
+    rating: req.body.city,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("calabar")
+    .replaceOne({ _id: calabarId }, rental);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while updating the user.");
+  }
+};
+
+const deleteRental = async (req, res) => {
+  const calabarId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("calabar")
+    .deleteOne({ _id: calabarId });
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while updating the user.");
+  }
+};
 module.exports = {
   getAll,
   getSingle,
+  createRental,
+  updateRental,
+  deleteRental,
 };
--- routes/calabar.js
+++ routes/calabar.js
@@ -2,8 +2,15 @@ const router = require("express").Router();
 
 const calabarController = require("../controllers/calabar");
 
+// endpoints
 router.get("/", calabarController.getAll);
 
 router.get("/:id", calabarController.getSingle);
+
+router.post("/", calabarController.createRental);
+
+router.put("/:id", calabarController.updateRental);
+
+router.post("/:id", calabarController.deleteRental);
 
 module.exports = router;
